8db60db4cd7e77aff7de095e73b6c31c296ef65e
Jai Israni <55907204+jaiisrani@users.noreply.github.com>
2021-10-18 22:01:27 +0530

    Add files via upload

---
+++ Command_pump/Command_pump.ino
@@ -0,0 +1,24 @@
+int pump_pin = ; // some analog pin
+int moisture_threshold = ; // value between 0 and 1023.
+//// Pump is ON while soil moisture content < moisture threshold
+
+float pump_speed; // placeholder for value between 0 and 255
+// Soil moisture variables:
+//// int moisture_level
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pump_pin, OUTPUT);
+}
+
+void loop() {
+  if (moisture_level < moisture_threshold) {
+    float initial_pump_speed = pump_speed;
+    // Have todesign proper function for pump_speed
+    pump_speed = (1 - (moisture_level / moisture_threshold)) * 255 - (initial_pump_speed / 10);
+    analogWrite(pump_pin, int(pump_speed));
+    delay(2000); // let pump run for two seconds 
+  }
+  analogWrite(pump_pin, 0);
+  delay(6000); // wait for six seconds to let water get evenly distributed in the soil
+}
